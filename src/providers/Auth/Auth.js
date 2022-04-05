b0ea5e31737ec613d62b6680ff51e7e0891cb5a4
Ryan McHenry <rmc@mbp15.local>
2022-04-05 16:54:16 -0400

    updating auth

--- src/providers/Auth/Auth.js
+++ src/providers/Auth/Auth.js
@@ -1,12 +1,14 @@
 import { Loading, Login } from 'components'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth, parseUserFromFirebaseResponse } from 'helpers'
+// import { useFirestore } from 'hooks'
 import React, { createContext, useEffect, useState } from 'react'
 
 export const AuthContext = createContext()
 
 export function Auth({ children }) {
   const [user, setUser] = useState()
+  // const profile = useFirestore('profiles', user ? user.uid : null)
 
   useEffect(() => {
     onAuthStateChanged(auth, currentUser => {
@@ -23,6 +25,8 @@ export function Auth({ children }) {
   if (user === undefined) return <Loading text={'Signing in...'} />
 
   if (!user) return <Login />
+
+  // if (!profile) return <EditProfile />
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
