bbafa7c03ce2c1e3c4ff946c1280842641a66940
Ryan McHenry <rmc@mbp15.local>
2022-04-05 16:47:06 -0400

    resolving merge conflicts

--- src/providers/Auth/Auth.js
+++ src/providers/Auth/Auth.js
@@ -1,4 +1,4 @@
-import { Loading } from 'components'
+import { Loading, Login } from 'components'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth, parseUserFromFirebaseResponse } from 'helpers'
 import React, { createContext, useEffect, useState } from 'react'
@@ -21,6 +21,8 @@ export function Auth({ children }) {
   // render a loading state until we can confirm
   // whether a user is signed in or not
   if (user === undefined) return <Loading text={'Signing in...'} />
+
+  if (!user) return <Login />
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
